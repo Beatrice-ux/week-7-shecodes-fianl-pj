84985e1b3517ee0391d91a1a7ee1be9e620e810d
Beatrice-ux <80289139+Beatrice-ux@users.noreply.github.com>
2021-09-20 16:22:47 +0100

    accurate date/time updates

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function formDate(timestamp){
+    let date=new Date(timestamp);
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day=days[date.getDay()];
+
+    return(`${day} ${hours}:${minutes}`);
+}
+
+
+
+
 function showTemperature(response){
     console.log(response.data);
     let placeElement=document.querySelector("#place");
@@ -5,12 +18,13 @@ function showTemperature(response){
     let descriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
+    let dateElement=document.querySelector("#date");
     placeElement.innerHTML=response.data.name;
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     descriptionElement.innerHTML=response.data.weather[0].description;
     humidityElement.innerHTML=response.data.main.humidity;
     windElement.innerHTML=Math.round(response.data.wind.speed);
-
+    dateElement.innerHTML= formDate(response.data.dt*1000)
 
 }
 
