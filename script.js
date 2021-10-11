0f9e3e0117807c16f567140b799cc7a323741181
Beatrice-ux <80289139+Beatrice-ux@users.noreply.github.com>
2021-10-11 13:40:28 +0100

    forecast loopv2

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function formDate(timestamp){
 
 function displayForecast (){
     let forecastElement=document.querySelector("#forecast");
-    let forecastHTML=`<div class="row">`;
     let days=["Tue", "Wed","Thu","Fri","Sat","Sun"];
+    let forecastHTML=`<div class="row">`;
     days.forEach(function(day){
-        forecastHTML=forecastHTML+`
-    
+        forecastHTML=forecastHTML+
+        `
         <div class="col-2">
-            <div class="weather-forecast-date">Thu</div>
+            <div class="weather-forecast-date">${day}</div>
                 <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
                     <div class="weather-forecast-temperatures">
                         <span class="weather-forecast-temperature-max"> 18° </span>
