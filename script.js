bdbe091be98cb2ffb882c6092e1d660d504eda1d
Beatrice-ux <80289139+Beatrice-ux@users.noreply.github.com>
2021-10-14 19:26:54 +0100

    Display the forecast data using API

--- script.js
+++ script.js
@@ -7,33 +7,46 @@ function formDate(timestamp){
     let minutes=date.getMinutes();
     let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let day=days[date.getDay()];
-
+    
     return(`${day} ${hours}:${minutes}`);
 }
 
-function displayForecast() {
+function formatDay(timestamp){
+    let date= new Date(timestamp*1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+
+function displayForecast(response) {
+    let forecast=response.data.daily;
     let forecastElement=document.querySelector("#forecast");
-    let days=["Tue", "Wed","Thu","Fri","Sat","Sun"];
+
     let forecastHTML=`<div class="row">`;
-    days.forEach(function(day){
+    forecast.forEach(function(forecastDay,index){
+    if (index < 6) {
         forecastHTML=forecastHTML+
         `
     <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
                     <div class="weather-forecast-temperatures">
-                        <span class="weather-forecast-temperature-max"> 18° </span>
-                        <span class="weather-forecast-temperature-min"> 12° </span>
+                        <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+                        <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
                     </div>
     </div>
     `;
+    }
     });
     
     forecastHTML=forecastHTML + `</div>`;
     forecastElement.innerHTML=forecastHTML;
-    console.log (forecastHTML);
+    
 }
 function getForecest(coordinates){
+    
     console.log(coordinates);
     let apiKey="a3b981fcdb00e192a7a49927e31c8d54";
     let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -60,7 +73,7 @@ function showTemperature(response){
     dateElement.innerHTML= formDate(response.data.dt*1000)
     iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
-    getForecast(response.data.coord);
+    getForecest(response.data.coord);
 
 }
 
@@ -105,4 +118,3 @@ let celciusLink=document.querySelector("#celcius");
 celciusLink.addEventListener("click",showCelcius);
 
 searchCity("Hong Kong");
-displayForecast();
